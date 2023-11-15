03dbb59524c8cefc64d94c6eba7b31d373da0296
Latika Verma <vermalatika8@gmail.com>
2023-11-15 18:29:39 +0530

    episode2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const parent = React.createElement('div', { id: 'parent' }, React.createElement('div', { id: 'child' }, [React.createElement('h1', {}, 'i am gradson h1 tag'), React.createElement('h2', {}, 'i am gradson h2 tag')]));
 
 var root = ReactDOM.createRoot(document.getElementById('root'));
