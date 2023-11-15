8d133c19513466ee9aa56174380f75a0dcc37ca7
Latika Verma <vermalatika8@gmail.com>
2023-11-15 19:41:49 +0530

    episode 3

--- App.js
+++ App.js
@@ -4,5 +4,10 @@ import ReactDOM from 'react-dom/client';
 const parent = React.createElement('div', { id: 'parent' }, React.createElement('div', { id: 'child' }, [React.createElement('h1', {}, 'i am gradson h1 tag'), React.createElement('h2', {}, 'i am gradson h2 tag')]));
 
 var root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(root);
 
-root.render(parent);
+//jsx (Transpiled before it reaches the JS) -Parcel - Babel
+//JSX => React.createElement => ReactElement-JS Object => HTML(render)
+const jsx = <h1>Through JSX elements</h1>;
+console.log(jsx);
+root.render(jsx);
